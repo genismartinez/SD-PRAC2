e1e06c2817bd7fd85fe24384904f2649f4043a9a
genismartinez <75421033+genismartinez@users.noreply.github.com>
2023-06-17 09:32:03 +0200

    Initial commit

---
+++ KVStore/kvstorage/kvstorage.py
@@ -0,0 +1,272 @@
+import time
+import random
+import uuid
+from typing import Dict, Union, List
+import logging
+import grpc
+from KVStore.protos.kv_store_pb2 import *
+from KVStore.protos.kv_store_pb2_grpc import KVStoreServicer, KVStoreStub
+from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
+
+from KVStore.protos.kv_store_shardmaster_pb2 import Role, QueryRequest
+
+EVENTUAL_CONSISTENCY_INTERVAL: int = 2
+
+logger = logging.getLogger("KVStore")
+
+
+class KVStorageService:
+
+    def __init__(self):
+        pass
+
+    def get(self, key: int) -> str:
+        pass
+
+    def l_pop(self, key: int) -> str:
+        pass
+
+    def r_pop(self, key: int) -> str:
+        pass
+
+    def put(self, key: int, value: str):
+        pass
+
+    def append(self, key: int, value: str):
+        pass
+
+    def redistribute(self, destination_server: str, lower_val: int, upper_val: int):
+        pass
+
+    def transfer(self, keys_values: list):
+        pass
+
+    def add_replica(self, server: str):
+        pass
+
+    def remove_replica(self, server: str):
+        pass
+
+
+class KVStorageSimpleService(KVStorageService):
+
+    def __init__(self):
+        super().__init__()
+        self.lower_bound = 0
+        self.upper_bound = 100
+        self._dictionary = dict()
+        self._locks = dict()    # lock = 1 -> franja crítica NO ocupada
+                                # lock = 0 -> franja crítica ocupada
+                                # wait -> while lock == 0: wait then, lock = 1
+                                # signal -> while lock == 1: signal then, lock = 0
+
+    def waitKey(self, key: int):
+        identifier = uuid.uuid4()  # Generamos un identificador único para cada petición
+        if key in self._locks:  # Si el identificador ya está en la lista de locks, no se puede acceder
+            self._locks[key].append(identifier)  # Añadimos el identificador a la lista de locks
+        else:
+            self._locks[key] = [identifier]  # Creamos una nueva lista de locks con el identificador
+        print("PROCESO: " + str(identifier) + " COLA ESPERA DE KEY: " + str(self._locks[key]) + " KEY: " + str(key))
+        while self._locks[key][0] != identifier:  # Mientras no sea el primer identificador de la lista
+            print("PROCESO: " + str(identifier) + " ESPERANDO A QUE SE LIBERE LA KEY: ")
+            time.sleep(1)  # Esperamos activamente == wait
+
+    def signalKey(self, key: int):
+        self._locks[key].pop(0)  # Eliminamos el identificador de la lista de locks == signal
+
+    def get(self, key: int) -> Union[str, None]:
+        self.waitKey(key)  # Esperamos activamente a que la key esté libre == wait
+        if key in self._dictionary:  # Si la key está en el diccionario
+            temp = self._dictionary[key]  # Guardamos el valor de la key temporalmente
+        else:
+            temp = None  # Si no está, devolvemos None
+        self.signalKey(key)  # Liberamos la key == signal
+        return temp  # Devolvemos el valor de la key
+
+
+
+    def l_pop(self, key: int) -> Union[str, None]:
+        try:
+            self.waitKey(key)   # Esperamos activamente == wait
+            if len(self._dictionary[key]) > 1:
+                char = self._dictionary[key][0]
+                self._dictionary[key] = self._dictionary[key][1:]
+            elif len(self._dictionary[key]) == 1:
+                char = self._dictionary[key][0]
+                self._dictionary[key] = ""
+            else:
+                char = ""
+            self.signalKey(key)  # Liberamos la key == signal
+            return char
+        except KeyError:
+            return None
+
+    def r_pop(self, key: int) -> Union[str, None]:
+        try:
+            self.waitKey(key)   # Esperamos activamente == wait
+            if len(self._dictionary[key]) > 1:
+                char = self._dictionary[key][-1]
+                self._dictionary[key] = self._dictionary[key][:-1]
+            elif len(self._dictionary[key]) == 1:
+                char = self._dictionary[key][0]
+                self._dictionary[key] = ""
+            else:
+                char = ""
+            self.signalKey(key)  # Liberamos la key == signal
+            return char
+        except KeyError:
+            return None
+
+    # Saves the value into the key. If the key already exists, it overwrites it.
+    def put(self, key: int, value: str):
+        self.waitKey(key)   # Esperamos activamente == wait
+        self._dictionary[key] = value
+        self.signalKey(key)  # Liberamos la key == signal
+
+    #   Concateates the specified value to the leftmost end of the value in the key.
+    #   If the key does not exist, it saves the value into the key.
+    def append(self, key: int, value: str):
+        print("APPEND: " + str(key) + " " + str(value))
+        self.waitKey(key)  # Esperamos activamente == wait
+        if key in self._dictionary:  # Si la key ya existe, concatenamos el valor
+            self._dictionary[key] = self._dictionary[key] + value
+        else:
+            self._dictionary[key] = value
+        print("DICCIONARIO: " + str(self._dictionary[key]))
+        self.signalKey(key)  # Liberamos la key == signal
+
+    def redistribute(self, destination_server: str, lower_val: int, upper_val: int):
+        # We build the dictionary with the matching keys
+        keys_values = dict()
+        lista = list[KeyValue]
+        for key in range(lower_val, upper_val):
+            if key in self._dictionary:
+                lista.append(KeyValue(key=key, value=self._dictionary[key]))
+
+                #keys_values[key] = self._dictionary[key]  # We add the key and its value to the dictionary
+        # We send the dictionary to the destination server
+
+        channel = grpc.insecure_channel(destination_server)
+        stub = KVStoreStub(channel)
+        # Generate gRPC request to TransferRequest
+
+
+        stub.Transfer(TransferRequest(keys_values=lista))
+
+    def transfer(self, keys_values: List[KeyValue]):
+        for key, value in keys_values:
+            self.waitKey(key)
+            self._dictionary[key] = value
+            self.signalKey(key)
+
+
+class KVStorageReplicasService(KVStorageSimpleService):
+    role: Role
+
+    def __init__(self, consistency_level: int):
+        super().__init__()
+        self.consistency_level = consistency_level
+        """
+        To fill with your code
+        """
+
+    def l_pop(self, key: int) -> str:
+        """
+        To fill with your code
+        """
+
+    def r_pop(self, key: int) -> str:
+        """
+        To fill with your code
+        """
+
+    def put(self, key: int, value: str):
+        """
+        To fill with your code
+        """
+
+    def append(self, key: int, value: str):
+        """
+        To fill with your code
+        """
+
+    def add_replica(self, server: str):
+        """
+        To fill with your code
+        """
+
+    def remove_replica(self, server: str):
+        """
+        To fill with your code
+        """
+
+    def set_role(self, role: Role):
+        logger.info(f"Got role {role}")
+        self.role = role
+
+
+class KVStorageServicer(KVStoreServicer):
+
+    def __init__(self, service: KVStorageService):
+        self.storage_service = service
+        """
+        To fill with your code
+        """
+
+    def Get(self, request: GetRequest, context) -> GetResponse:
+        key = request.key
+        response = self.storage_service.get(key)
+        get_response = GetResponse()
+        if response is not None:
+            get_response.value = response
+        return get_response
+
+    def LPop(self, request: GetRequest, context) -> GetResponse:
+        key = request.key
+        response = self.storage_service.l_pop(key)
+        get_response = GetResponse()
+        if response is not None:
+            get_response.value = response
+        return get_response
+
+    def RPop(self, request: GetRequest, context) -> GetResponse:
+        key = request.key
+        response = self.storage_service.r_pop(key)
+        get_response = GetResponse()
+        if response is not None:
+            get_response.value = response
+        return get_response
+
+    def Put(self, request: PutRequest, context) -> google_dot_protobuf_dot_empty__pb2.Empty:
+        key = request.key
+        value = request.value
+        self.storage_service.put(key, value)
+        return google_dot_protobuf_dot_empty__pb2.Empty()
+
+    def Append(self, request: AppendRequest, context) -> google_dot_protobuf_dot_empty__pb2.Empty:
+        key = request.key
+        value = request.value
+        self.storage_service.append(key, value)
+        return google_dot_protobuf_dot_empty__pb2.Empty()
+
+    def Redistribute(self, request: RedistributeRequest, context) -> google_dot_protobuf_dot_empty__pb2.Empty:
+        destination_server = request.destination_server
+        lower_val = request.lower_val
+        upper_val = request.upper_val
+        self.storage_service.redistribute(destination_server, lower_val, upper_val)
+        return google_dot_protobuf_dot_empty__pb2.Empty()
+
+    def Transfer(self, request: TransferRequest, context) -> google_dot_protobuf_dot_empty__pb2.Empty:
+        keys_values = request.keys_values
+        self.storage_service.transfer(list(keys_values))
+        return google_dot_protobuf_dot_empty__pb2.Empty()
+
+    def AddReplica(self, request: ServerRequest, context) -> google_dot_protobuf_dot_empty__pb2.Empty:
+        server = request.server
+        self.storage_service.add_replica(server)
+        return google_dot_protobuf_dot_empty__pb2.Empty()
+
+    def RemoveReplica(self, request: ServerRequest, context) -> google_dot_protobuf_dot_empty__pb2.Empty:
+        server = request.server
+        self.storage_service.remove_replica(server)
+        return google_dot_protobuf_dot_empty__pb2.Empty()
---
+++ KVStore/shardmaster/shardmaster.py
@@ -0,0 +1,141 @@
+import logging
+import threading
+import grpc
+
+from KVStore.tests.utils import KEYS_LOWER_THRESHOLD, KEYS_UPPER_THRESHOLD
+from KVStore.protos.kv_store_pb2 import RedistributeRequest, ServerRequest
+from KVStore.protos.kv_store_pb2_grpc import KVStoreStub
+from KVStore.protos.kv_store_shardmaster_pb2_grpc import ShardMasterServicer
+from KVStore.protos.kv_store_shardmaster_pb2 import *
+
+from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
+
+logger = logging.getLogger(__name__)
+
+
+class ShardMasterService:
+    def join(self, server: str):
+        pass
+
+    def leave(self, server: str):
+        pass
+
+    def query(self, key: int) -> str:
+        pass
+
+    def join_replica(self, server: str) -> Role:
+        pass
+
+    def query_replica(self, key: int, op: Operation) -> str:
+        pass
+
+
+class ShardMasterSimpleService(ShardMasterService):
+    def __init__(self):
+        self.server_list = []  # We will save a tuple 3 arguments: (port, lower_bound, upper_bound)
+        self.assigned_ranges = dict()  # We will save a tuple 2 arguments: (lower_bound, upper_bound)
+
+    def join(self, server: str):
+        self.server_list.append(server)
+        #self.rebalance()
+
+    def leave(self, server: str):
+        self.server_list.remove(server)
+        #self.rebalance()
+
+    def query(self, key: int) -> str:
+        for server in self.assigned_ranges:  # server = (port, lower_bound, upper_bound)
+            if self.assigned_ranges[server][0] <= key < self.assigned_ranges[server][1]:  # If the key is in the range of the server
+                return server  # return port
+
+    # Redistribute all keys to all servers to ensure all the keys are in the right server
+    def _rebalance(self):
+        # Compute chunk size
+        shard_size = (KEYS_UPPER_THRESHOLD - KEYS_LOWER_THRESHOLD) // len(self.server_list)
+        shard_remain = (KEYS_UPPER_THRESHOLD - KEYS_LOWER_THRESHOLD) % len(self.server_list)
+
+        # Compute the new distribution of keys before redistributing them
+        self.assigned_ranges = dict()
+        i=0
+        for server in self.server_list:
+            if shard_remain > 0:
+                current_shard_size  = shard_size + 1
+                shard_remain -= 1
+            else:
+                current_shard_size = shard_size
+
+            self.assigned_ranges[i]=(i * current_shard_size, current_shard_size * (i + 1))   # assigned_ranges = {0: (0, 33), 1: (33, 66), 2: (66, 100)}
+            i+=1
+
+        for origin_server in self.server_list: # For each server in the list we will redistribute the keys to the other servers
+            for destination_server in self.server_list:
+                if origin_server != destination_server: # We don't want to redistribute the keys to the same server
+                    channel = grpc.insecure_channel(origin_server)  # Create a channel to the origin server
+                    stub = KVStoreStub(channel) # Create a stub to the origin server
+                    # Generate gRPC request to RedistributeRequest
+                    stub.Redistribute(RedistributeRequest(destination_server=destination_server,
+                                                          lower_val=self.assigned_ranges[destination_server][0],
+                                                          upper_val=self.assigned_ranges[destination_server][1]))
+                    channel.close() # Close the channel to the origin server
+
+class ShardMasterReplicasService(ShardMasterSimpleService):
+    def __init__(self, number_of_shards: int):
+        super().__init__()
+        """
+        To fill with your code
+        """
+
+    def leave(self, server: str):
+        """
+        To fill with your code
+        """
+
+    def join_replica(self, server: str) -> Role:
+        """
+        To fill with your code
+        """
+
+    def query_replica(self, key: int, op: Operation) -> str:
+        """
+        To fill with your code
+        """
+
+
+class ShardMasterServicer(ShardMasterServicer):
+    def __init__(self, shard_master_service: ShardMasterService):
+        self.shard_master_service = shard_master_service
+
+    def Join(self, request: JoinRequest, context) -> google_dot_protobuf_dot_empty__pb2.Empty():
+        server = request.server
+        self.shard_master_service.join(server)
+        return google_dot_protobuf_dot_empty__pb2.Empty()
+
+    def Leave(self, request: LeaveRequest, context) -> google_dot_protobuf_dot_empty__pb2.Empty():
+        server = request.server
+        self.shard_master_service.leave(server)
+        return google_dot_protobuf_dot_empty__pb2.Empty()
+
+    def Query(self, request: QueryRequest, context) -> QueryResponse:
+        key = request.key
+        response = self.shard_master_service.query(key)
+        query_response = QueryResponse()
+        if response is not None:
+            query_response.server = response
+        return query_response
+
+
+    def JoinReplica(self, request: JoinRequest, context) -> JoinReplicaResponse:
+        #server = request.server
+        #role = self.shard_master_service.join_replica(server)
+        #return JoinReplicaResponse(role)
+        pass
+
+    def QueryReplica(self, request: QueryReplicaRequest, context) -> QueryResponse:
+        #key = request.key
+        #op = request.operation
+        #response = self.shard_master_service.query_replica(key, op)
+        #query_response = QueryResponse()
+        #if response is not None:
+        #    query_response.server = response
+        #return query_response
+        pass
